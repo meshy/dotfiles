c9bcb4e6421663399672903a29b4023c3ea4076d
Charlie Denton <charlie@meshy.co.uk>
2013-03-25 16:58:48 +0000

    Add `tig` to apt packages.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@
 sudo apt-get remove unity-lens-shopping unity-scope-musicstores
 
 # Install packages
-sudo apt-get install git python-dev build-essential libjpeg-dev libpng12-dev zsh gnome-shell synapse gparted gimp ubuntu-restricted-extras vlc ttf-mscorefonts-installer sqlite ruby quicksynergy postgresql-server-dev-all ack-grep sparkleshare ia32-libs memcached libmemcached-dev nautilus-open-terminal
+sudo apt-get install git tig python-dev build-essential libjpeg-dev libpng12-dev zsh gnome-shell synapse gparted gimp ubuntu-restricted-extras vlc ttf-mscorefonts-installer sqlite ruby quicksynergy postgresql-server-dev-all ack-grep sparkleshare ia32-libs memcached libmemcached-dev nautilus-open-terminal
 
 # Install zsh
 # wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
