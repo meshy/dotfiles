5effef38d53e83f4707b6eb35ff7de9efc77de2d
Charlie Denton <charlie@meshy.co.uk>
2021-07-22 00:06:16 +0100

    Move ZSH history config into own file

---
+++ history.zsh
@@ -0,0 +1,6 @@
+SAVEHIST=1000000
+HISTSIZE=1000000
+HISTFILE=${HOME}/.zsh_history
+# Enable timestamps in history
+setopt extendedhistory
+setopt share_history
--- zshrc
+++ zshrc
@@ -1,5 +1,6 @@
 source ~/.aliases.zsh
 source ~/.completion.zsh
 source ~/.keybindings.zsh
+source ~/.history.zsh
 eval "$(starship init zsh)"
 source ~/.sh_rc
